b777ed953582c90d6c3ea94ac1a4808d0df7e037
Lucila Carlos <lucicarl1401@gmail.com>
2024-05-08 13:27:42 -0400

    code

---
+++ PixelCode/photos.js
@@ -0,0 +1,123 @@
+var pixel1={
+    name:"Pixel 1",
+    image: "pixel_1.gif",
+    generation:"Generation: 1" ,
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset High Strength Concrete Mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "1",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel2={
+    name:"Pixel 2",
+    image: "pixel_2.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel3={
+    name:"Pixel 3",
+    image: "pixel_2.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "n/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel5={
+    name:"Pixel 5",
+    image: "pixel_5.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel9={
+    name:"Pixel 9",
+    image: "pixel_9.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel12={
+    name:"Pixel 12",
+    image: "pixel_12.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel13={
+    name:"Pixel 13",
+    image: "pixel_13.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel14={
+    name:"Pixel 14",
+    image: "pixel_14.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel16={
+    name:"Pixel 16",
+    image: "pixel_16.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel20={
+    name:"Pixel 20",
+    image: "pixel_20.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+var pixel23={
+    name:"Pixel 23",
+    image: "pixel_23.gif",
+    generation: "Generation: 1",
+    fc: "fc': 35",
+    concrete: "Concrete Mix: Rapidset high strength concrete mix",
+    fiber: "Fiber type: Glass", 
+    configurations: "N/A",
+    dosage: "Fiber Dosage: 3%"
+};
+
+
+
